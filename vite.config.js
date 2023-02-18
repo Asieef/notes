ad1d586910097e77dcb680dab74d094b0bfaf6ac
Asieef <asieef.chowdhury@gmail.com>
2023-02-18 12:16:33 +0600

    ase

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,8 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.NODE_ENV === "production" ? "/[notes-live]/" : "/",
+  base: process.env.NODE_ENV === "production" ? "/notes-live/" : "/",
+  build: {},
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
